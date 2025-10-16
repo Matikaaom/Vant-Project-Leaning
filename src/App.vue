7968a99560edaddc801b37809cac7bd0d3e74e4f
Matika Kraisaksri < GitHubkraisaksri121108@gmail.com>
2025-10-16 13:35:35 +0700

    refactor: enforce user login during LIFF initialization; prevent content loading for non-logged-in users

--- src/App.vue
+++ src/App.vue
@@ -112,17 +112,37 @@ const LIFF_ID = "2008284940-aZ5dYpXy"
 // })
 
 // const LIFF_ID = "2008284940-aZ5dYpXy";
+
 onMounted(async () => {
   try {
     await liff.init({ liffId: LIFF_ID });
+
+    if (!liff.isLoggedIn()) {
+      // บังคับให้ user login
+      liff.login();
+      return; // stop ต่อไม่ให้โหลดเนื้อหา
+    }
+
+    // ถ้า login แล้ว
     const profile = await liff.getProfile();
-    console.log(profile)
+    console.log(profile);
+
   } catch (error) {
-    console.log('error', error);
     console.error('LIFF initialization failed', error);
   }
-  
 });
+
+// onMounted(async () => {
+//   try {
+//     await liff.init({ liffId: LIFF_ID });
+//     const profile = await liff.getProfile();
+//     console.log(profile)
+//   } catch (error) {
+//     console.log('error', error);
+//     console.error('LIFF initialization failed', error);
+//   }
+  
+// });
 
 // const router = useRouter()
 
