4b82a71def9ee92e2af4613a26642dda09e357ad
Matika Kraisaksri < GitHubkraisaksri121108@gmail.com>
2025-10-17 17:06:24 +0700

    fix route 1

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,28 +1,5 @@
-// import { createRouter, createWebHistory } from 'vue-router'
-// import AppLayout from '@/components/AppLayout.vue'
-// import HomePage from '@/components/views/HomePage.vue'
-// import StepsPage from '@/components/views/StepsPage.vue'
-// import DashboardPage from '@/components/views/DashboardPage.vue'
-// import UserPage from '@/components/views/UserPage.vue'
-// import Checkline from '@/components/views/CheckLine.vue'
-// import liff from "@line/liff"
-
-// const routes = [
-//   // { path: '/', component: AppLayout },
-//   { path: '/', name: 'home', component: HomePage },
-//   { path: '/line', name: 'line', component: Checkline} ,
-//   { path: '/steps', name: 'steps', component: StepsPage },
-//   { path: '/dashboard', name: 'dashboard', component: DashboardPage },
-//   { path: '/user', name: 'user', component: UserPage }
-// ]
-
-// const router = createRouter({
-//   history: createWebHistory(),
-//   routes
-// })
-
 import { createRouter, createWebHistory } from 'vue-router'
-
+import AppLayout from '@/components/AppLayout.vue'
 import HomePage from '@/components/views/HomePage.vue'
 import StepsPage from '@/components/views/StepsPage.vue'
 import DashboardPage from '@/components/views/DashboardPage.vue'
@@ -30,24 +7,48 @@ import UserPage from '@/components/views/UserPage.vue'
 import Checkline from '@/components/views/CheckLine.vue'
 
 const routes = [
- { path: '/', name: 'home', component: HomePage },
- { path: '/line', name: 'line', component: Checkline },
- { path: '/steps', name: 'steps', component: StepsPage },
- { path: '/dashboard', name: 'dashboard', component: DashboardPage },
- { path: '/user', name: 'user', component: UserPage },
+  // { path: '/', component: AppLayout },
+  { path: '/', name: 'home', component: HomePage },
+  { path: '/line', name: 'line', component: Checkline} ,
+  { path: '/steps', name: 'steps', component: StepsPage },
+  { path: '/dashboard', name: 'dashboard', component: DashboardPage },
+  { path: '/user', name: 'user', component: UserPage }
 ]
 
 const router = createRouter({
- history: createWebHistory(),
- routes
-})
-
-router.beforeEach((to, from, next) => {
-  
-  next()
+  history: createWebHistory(),
+  routes
 })
 
 export default router
+
+// import { createRouter, createWebHistory } from 'vue-router'
+
+// import HomePage from '@/components/views/HomePage.vue'
+// import StepsPage from '@/components/views/StepsPage.vue'
+// import DashboardPage from '@/components/views/DashboardPage.vue'
+// import UserPage from '@/components/views/UserPage.vue'
+// import Checkline from '@/components/views/CheckLine.vue'
+
+// const routes = [
+//  { path: '/', name: 'home', component: HomePage },
+//  { path: '/line', name: 'line', component: Checkline },
+//  { path: '/steps', name: 'steps', component: StepsPage },
+//  { path: '/dashboard', name: 'dashboard', component: DashboardPage },
+//  { path: '/user', name: 'user', component: UserPage },
+// ]
+
+// const router = createRouter({
+//  history: createWebHistory(),
+//  routes
+// })
+
+// router.beforeEach((to, from, next) => {
+  
+//   next()
+// })
+
+// export default router
 
 
 // import { createRouter, createWebHistory } from 'vue-router'
